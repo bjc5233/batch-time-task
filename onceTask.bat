25a52df06290ba767e65a5fe33e8c0ef46211d87
bjc5233 <bjc5233@gmail.com>
2018-01-15 04:56:11 +0800

    some

--- onceTask.bat
+++ onceTask.bat
@@ -1,4 +1,4 @@
-@echo off& call lib\load.bat _uniqueStr& setlocal enabledelayedexpansion
+@echo off& call load.bat _uniqueStr& setlocal enabledelayedexpansion
 title 创建一次性计划任务
 set /p file=拖入文件:
 if not exist %file% (
